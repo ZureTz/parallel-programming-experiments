78e2c2a059106586c8f69b91f7afad1774bc8d5f
ZureTz <2819058133@qq.com>
2024-12-19 14:19:55 +0800

    优化实验4中的BFS测试数据和执行脚本，增加了对大规模图的测试用例，更新了执行时间记录

--- experiment-4/bfs/test.sh
+++ experiment-4/bfs/test.sh
@@ -9,13 +9,32 @@ make -C src all
 
 cd src
 
-# execution using different arguments
-./bfs-exec 0 100000 1000000
-./bfs-exec 0 100000 10000000
-./bfs-exec 0 100000 100000000
-./bfs-exec 0 1000000 1000000
-./bfs-exec 0 1000000 10000000
-./bfs-exec 0 1000000 100000000
-./bfs-exec 0 10000000 10000000
-./bfs-exec 0 10000000 100000000
-./bfs-exec 0 10000000 100000000
+# Define arrays for test parameters
+vertices=(100000 1000000 10000000 50000000)
+edges=(1000000 10000000 100000000 500000000)
+start_vertex=0
+
+# Nested loops for combinations
+for v in "${vertices[@]}"; do
+    for e in "${edges[@]}"; do
+        # Only run if edges are more than vertices (valid graph)
+        if [ $e -gt $v ]; then
+            echo "Testing with vertices=$v edges=$e"
+            ./bfs-exec $start_vertex $v $e
+            sleep 1  # Small delay between tests
+        fi
+    done
+done
+
+# Additional specific test cases for larger graphs
+large_cases=(
+    "100000000 1000000000"
+    "100000000 2000000000"
+)
+
+for case in "${large_cases[@]}"; do
+    read -r v e <<< "$case"
+    echo "Testing large graph with vertices=$v edges=$e"
+    ./bfs-exec $start_vertex $v $e
+    sleep 2  # Longer delay for larger tests
+done
